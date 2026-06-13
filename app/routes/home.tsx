0058c4bf8bf2731ac4c487b80e2819ea2dc5a650
amir830047agh@gmail.com <amir830047agh@gmail.com>
2026-06-14 00:20:52 +0330

    start of first page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer" },
+    { name: "description", content: "smart and intelligance resume" },
   ];
 }
 
+
 export default function Home() {
-  return <Welcome />;
+  return <>
+  <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+  <section className="main-section">
+    <div className="page-heading">
+      <h1>Track Your Applications</h1>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+    </div>
+  </section>
+  </main>
+  </>
 }
