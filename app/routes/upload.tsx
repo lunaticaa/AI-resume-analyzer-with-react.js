3b041737a175a54f4ce37f2d2695d8555684a611
amir830047agh@gmail.com <amir830047agh@gmail.com>
2026-06-23 22:36:17 +0330

    converting pdf to image

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,9 +1,14 @@
 import { type FormEvent , useState } from 'react'
+import { useNavigate } from 'react-router';
 import FileUploader from '~/components/FileUploader';
 import Navbar from '~/components/Navbar'
+import { convertPdfToImage } from '~/lib/pdf2Image';
+import { usePuterStore } from '~/lib/puter';
 
 
 const upload = () => {
+  const {auth, isLoading, fs, ai, kv} = usePuterStore();
+  const navigate = useNavigate();
   const [isProcessing, setIsProcessing] = useState(false);
   const [statusText, setStatusText] = useState('');
   const [file, setFile] = useState<File | null>(null);
@@ -11,20 +16,31 @@ const upload = () => {
     setFile(file)
   }
 
+  // handle Analyzer
+  const handleAnalyze = async ({companyName, jobTitle, jobDescription, file}: {companyName: string, jobTitle: string, jobDescription: string, file:File}) => {
+      setIsProcessing(true);
+      setStatusText('Uploading the file...');
+      const uploadedFile = await fs.upload([file]);
+
+      if(!uploadedFile) return setStatusText('Error: Failed to upload file');
+
+      setStatusText('Converting to image...');
+      const imageFile = await convertPdfToImage(file);
+
+  }
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) =>{
     e.preventDefault();
+    // target the value of form
     const form = e.currentTarget.closest('form');
     if(!form) return;
     const formData = new FormData(form);
-    const companyName = formData.get('company-name');
-    const jobTitle = formData.get('job-title');
-    const jobDescription = formData.get('job-description');
+    const companyName = formData.get('company-name') as string;
+    const jobTitle = formData.get('job-title') as string;
+    const jobDescription = formData.get('job-description') as string;
 
-    console.log({
-      companyName,
-      jobTitle,
-      jobDescription
-    });
+    if(!file) return;
+    handleAnalyze({companyName, jobTitle, jobDescription, file})
     
   }
   return (
@@ -71,3 +87,7 @@ const upload = () => {
 }
 
 export default upload
+function convertPdfImage(file: File) {
+  throw new Error('Function not implemented.');
+}
+
