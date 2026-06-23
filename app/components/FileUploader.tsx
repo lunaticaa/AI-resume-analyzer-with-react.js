5b974f6f5c8c8e3ef580970ebf565fb25260f1fd
amir830047agh@gmail.com <amir830047agh@gmail.com>
2026-06-23 15:06:15 +0330

    uploader icon info.svg

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
 
 const FileUploader = () => {
+  const onDrop = useCallback(acceptedFiles => {
+
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
   return (
     <div className='w-full gradient-border'>
-      uploader
+       <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      <div className='space-y-4 cursor-pointer'>
+        <div className="mx-auto w-16 h-16 items-center justify-center flex">
+          <img src="/icons/info.svg" alt="upload" className='size-20' />
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
